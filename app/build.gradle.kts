a3c49a5831f4e0af08292c8b7f287d921c5caff9
rt01rj-cyber <rt01rj@gmail.com>
2026-09-25 03:54:20 +0100

    Bump Green Hustle to v0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "uk.co.greenhustle.cardgame"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     buildTypes {
